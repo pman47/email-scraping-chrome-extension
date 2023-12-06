f4e781dba9565bf390865e6c224c1915dbd04b7f
Manish Prajapati <manishprajapatim416@gmail.com>
2023-12-07 00:47:08 +0530

    Did some ui changes

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 // Button
 const scrapEmails = document.getElementById("scrapEmails");
+const list = document.getElementById("emailList");
+const emailContainer = document.getElementById("emailContainer");
 
 scrapEmails.addEventListener("click", async () => {
-  console.log("Clicked");
-
   let [tab] = await chrome.tabs.query({
     active: true,
     currentWindow: true,
@@ -30,3 +30,31 @@ function scrapEmailsFromPage() {
   // send emails to popup
   chrome.runtime.sendMessage({ emails });
 }
+
+// Handler to receive emails from content script
+chrome.runtime.onMessage.addListener((request) => {
+  list.innerHTML = "";
+  // Get emails
+  let emails = request.emails;
+
+  // Display emails on popup
+  if (emails === null || emails.length === 0) {
+    let li = document.createElement("li");
+    li.innerText = "No email found.";
+    list.appendChild(li);
+  } else {
+    // Create a Set to store unique emails
+    let uniqueEmailsSet = new Set();
+
+    // Iterate through the emails array
+    emails.forEach((email) => {
+      if (!uniqueEmailsSet.has(email)) {
+        uniqueEmailsSet.add(email);
+
+        let li = document.createElement("li");
+        li.innerText = email;
+        list.appendChild(li);
+      }
+    });
+  }
+});
